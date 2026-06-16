a7ba3053f86987480e95396d7ed99ac87e98a765
StasX <s.mestechkin@gmail.com>
2026-06-16 18:34:54 +0300

    test

--- init.sh
+++ init.sh
@@ -19,7 +19,6 @@ export KUBECONFIG=~/.kube/config
 echo "=== Waiting for nodes to be ready ==="
 kubectl wait --for=condition=Ready node --all --timeout=300s
 
-
 # helm repo add argo https://argoproj.github.io/argo-helm
 # helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 # helm repo add grafana https://grafana.github.io/helm-charts
