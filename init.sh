2ab01784d6c15a28d10ebce60c4a18e4444b7e13
StasX <s.mestechkin@gmail.com>
2026-06-16 18:28:04 +0300

    test

--- init.sh
+++ init.sh
@@ -19,13 +19,6 @@ export KUBECONFIG=~/.kube/config
 echo "=== Waiting for nodes to be ready ==="
 kubectl wait --for=condition=Ready node --all --timeout=300s
 
-# echo "=== Installing Helm ==="
-# curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-# chmod 700 get_helm.sh
-# ./get_helm.sh
-
-# Rest of your script with error checking...
-
 
 # helm repo add argo https://argoproj.github.io/argo-helm
 # helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
