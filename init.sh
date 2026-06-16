ac4312d988fe13a33b224e5fc932626230c1080d
StasX <s.mestechkin@gmail.com>
2026-06-16 18:42:30 +0300

    test

--- init.sh
+++ init.sh
@@ -1,19 +1,15 @@
-echo "=== Install k3s with built-in Traefik ==="
 curl -sfL https://get.k3s.io | sh -
 
-echo "=== Waiting for K3s service to start ==="
 until systemctl is-active --quiet k3s; do
     echo "Waiting for k3s service..."
     sleep 2
 done
 
-echo "=== Prepare kubeconfig ==="
 mkdir -p ~/.kube
 sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
 sudo chown $(id -u):$(id -g) ~/.kube/config
 export KUBECONFIG=~/.kube/config
 
-echo "=== Waiting for nodes to be ready ==="
 kubectl wait --for=condition=Ready node --all --timeout=300s
 
 # helm repo add argo https://argoproj.github.io/argo-helm
