6bbe7c755588f93779087716b3bd2d3b5dc5df09
StasX <s.mestechkin@gmail.com>
2026-06-16 15:30:31 +0300

    test

--- init.sh
+++ init.sh
@@ -14,9 +14,7 @@ export KUBECONFIG=~/.kube/config
 
 sudo kubectl --kubeconfig=/etc/rancher/k3s/k3s.yaml wait --for=condition=Ready node --all --timeout=300s
 
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
-sudo chmod 700 get_helm.sh
-bash ./get_helm.sh
+sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4 | bash
 
 # helm repo add argo https://argoproj.github.io/argo-helm
 # helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
